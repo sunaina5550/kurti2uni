c241f8465397b85e857dc68f7bde8b07cfb90430
sunaina5550 <114802032+sunaina5550@users.noreply.github.com>
2022-10-02 05:41:38 +0530

    Add files via upload

---
+++ custom9.js
@@ -0,0 +1,75 @@
+function exportHTML(){
+
+       var header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
+
+            "xmlns:w='urn:schemas-microsoft-com:office:word' "+
+
+            "xmlns='http://www.w3.org/TR/REC-html40'>"+
+
+            "<head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
+
+       var footer = "</body></html>";
+
+       var sourceHTML = header+document.getElementById("chanakya_text").value+footer;
+
+       
+
+       var source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
+
+       var fileDownload = document.createElement("a");
+
+       document.body.appendChild(fileDownload);
+
+       fileDownload.href = source;
+
+       fileDownload.download = 'chanakyafont-krutidevunicodeoconverter.doc';
+
+       fileDownload.click();
+
+       document.body.removeChild(fileDownload);
+
+    }
+
+function sendtowhatsapp()
+
+{
+
+	var texter = document.getElementById("unicode_text").value;
+
+	window.open('https://wa.me/?text='+texter, '_blank');
+
+}
+
+function sendtomail()
+
+{
+
+	var texter = document.getElementById("unicode_text").value;
+
+	window.open('https://mail.google.com/mail/u/0/?fs=1&tf=cm&body='+texter, '_blank');
+
+}
+
+function copyclipboard() {
+
+   var copyText = document.querySelector("#chanakya_text");
+
+   copyText.select();
+
+   copyText.setSelectionRange(0, 99999);
+
+   document.execCommand("copy");
+
+   alert("Text Copied To Clipboard.");
+
+}
+
+function cleartext() {
+
+document.getElementById("krutitext").value = '';
+
+	document.getElementById("unicodetext").value = '';
+	
+	document.getElementById("chanakya_text").value = '';
+
+}
